6e6d9b565286af444d62844acdfef0baedca26f1
svitorz <vitor.fabio.castro@gmail.com>
2025-08-06 11:14:28 -0300

    feat: funcao para printar com janelas

--- Aula1/Cliente.java
+++ Aula1/Cliente.java
@@ -1,5 +1,7 @@
 package Aula1;
 
+import javax.swing.JOptionPane;
+
 public class Cliente {
 
   private String nome, rg;
@@ -8,13 +10,17 @@ public class Cliente {
     this.nome = nome;
     this.rg = rg;
 
-    System.out.println("O nome informado é " + this.nome + " e o RG informado é: " + this.rg);
+    this.printMessage("O nome informado é " + this.nome + " e o RG informado é: " + this.rg);
   }
 
   public void mostrarNome(String nome) {
     this.nome = nome;
 
-    System.out.println("O nome informado foi: " + this.nome);
+    this.printMessage("O nome informado foi: " + this.nome);
+  }
+
+  private void printMessage(String message) {
+    JOptionPane.showMessageDialog(null, message);
   }
 }
 
