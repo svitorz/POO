7d1cd341317eabf6612b430c879238446af9bad1
svitorz <vitor.fabio.castro@gmail.com>
2025-08-13 11:11:23 -0300

    feat: exercicio para calcular salario

--- Exercicio3/PrincipalSalario.java
+++ Exercicio3/PrincipalSalario.java
@@ -5,10 +5,14 @@ import javax.swing.JOptionPane;
 public class PrincipalSalario {
   public static void main(String args[]) {
       Salario salario = new Salario();
-
-    JOptionPane.showMessageDialog(null, "O novo salário é de: " + salario.calcularAumento(
-      Double.parseDouble(JOptionPane.showInputDialog("Insira o salário atual: ")),
-          Double.parseDouble(JOptionPane.showInputDialog("Insira a porcentagem de aumento do salário: "))
-    ));
+    
+    try {
+      JOptionPane.showMessageDialog(null, "O novo salário é de: " + salario.calcularAumento(
+        Double.parseDouble(JOptionPane.showInputDialog("Insira o salário atual: ")),
+            Double.parseDouble(JOptionPane.showInputDialog("Insira a porcentagem de aumento do salário: "))
+      ));
+    } catch (Exception e) {
+      JOptionPane.showMessageDialog(null, "Formato inválido: " + e.getMessage());
+    }
   } 
 }
